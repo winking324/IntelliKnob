b6b446ee816f01c973b86bc80dcfbc9494bea479
ShanHui <shanhui@agora.io>
2022-08-17 19:41:24 +0800

    some base cpp feature codes

---
+++ IntelliKnob/src/pkg/base/cpp_feature.h
@@ -0,0 +1,35 @@
+// Copyright (c) winking324
+// Base cpp features
+//
+
+#pragma once  // NOLINT(build/header_guard)
+
+namespace intelliknob {
+namespace base {
+
+class NonCopyOrMovable {
+ protected:
+  NonCopyOrMovable() {}
+  ~NonCopyOrMovable() {}
+
+ private:
+  NonCopyOrMovable(const NonCopyOrMovable &) = delete;
+  NonCopyOrMovable(NonCopyOrMovable &&) = delete;
+  NonCopyOrMovable &operator=(const NonCopyOrMovable &) = delete;
+  NonCopyOrMovable &operator=(NonCopyOrMovable &&) = delete;
+};
+
+template <typename T>
+class Singleton : private NonCopyOrMovable {
+ public:
+  static T *Instance() {
+    static T inst;
+    return &inst;
+  }
+
+  Singleton() = delete;
+  ~Singleton() = delete;
+};
+
+}  // namespace base
+}  // namespace intelliknob
